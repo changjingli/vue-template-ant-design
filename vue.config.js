f2474b602f169aeae76e74495d1217b9456e52ea
Jingli Chang <jingli.chang01@gmail.com>
2020-03-04 15:17:46 +0800

    chore: add zip-webpack-plugin for production mode

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,27 @@
-const BundleAnalyzerPlugin = require(
-  'webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require(
+//   'webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
+const zipWebpackPlugin = require('zip-webpack-plugin');
+const env = process.env.NODE_ENV;
+const {name, version} = require('./package');
+
+const basePlugin = [
+  new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/),
+];
+const devPlugin = [];
+const prodPlugin = [
+  new zipWebpackPlugin({
+    filename: `${ name }_${ version }_${ env }.zip`,
+  }),
+];
+
+const plugins = env === 'development'
+  ? basePlugin.concat(devPlugin)
+  : basePlugin.concat(prodPlugin);
 
 module.exports = {
   publicPath: './',
   configureWebpack: {
-    plugins: [
-      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/),
-      new BundleAnalyzerPlugin(),
-    ],
+    plugins,
   },
 };
